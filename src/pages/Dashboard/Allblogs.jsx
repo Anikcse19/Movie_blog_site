76429b105d8ecd490336e574ac9c04d64438aab4
Anik Deb <anikdebcse@gmail.com>
2024-04-07 20:27:19 +0600

    date filtered added

--- src/pages/Dashboard/Allblogs.jsx
+++ src/pages/Dashboard/Allblogs.jsx
@@ -10,62 +10,67 @@ import base_url from "../../Utils/Url";
 import DashboardLayout from "../../components/Layout/DashboardLayout";
 import BlogDeleteModal from "../../components/Shared/Modal/BlogDeleteModal";
 
-const token=localStorage.getItem('token')
+const token = localStorage.getItem("token");
 const AllBlogsPage = () => {
   const router = useNavigate();
 
-  const [articles,setArticles]=useState([])
-  const [categories,setCategories]=useState([])
-  const [selectedCategory,setSelectedCategory]=useState('')
-  const [currentSelectedArticle,setCurrentSelectedArticle]=useState({})
-  const {isDeleteModalOpen,setIsDeleteModalOpen,allBlogDelete,setAllBlogDelete}=useContext(BlogContext)
+  const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [currentSelectedArticle, setCurrentSelectedArticle] = useState({});
+  const {
+    isDeleteModalOpen,
+    setIsDeleteModalOpen,
+    allBlogDelete,
+    setAllBlogDelete,
+  } = useContext(BlogContext);
 
-
-
-  const fetchArticles=()=>{
+  const fetchArticles = () => {
     axios
-      .get(`${base_url}/articles${selectedCategory=="" ? "":`?category_id=${selectedCategory}`}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
+      .get(
+        `${base_url}/articles${
+          selectedCategory == "" ? "" : `?category_id=${selectedCategory}`
+        }`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
       .then((res) => setArticles(res.data.articles));
-  }
+  };
 
   useEffect(() => {
-    fetchArticles()
-    axios.get(`${base_url}/categories`).then(res=>setCategories(res?.data?.categories))
-  }, [isDeleteModalOpen.value,selectedCategory,allBlogDelete]);
+    fetchArticles();
+    axios
+      .get(`${base_url}/categories`)
+      .then((res) => setCategories(res?.data?.categories));
+  }, [isDeleteModalOpen.value, selectedCategory, allBlogDelete]);
 
+  const allDates = articles?.map((article) => article?.created_at);
 
-  
- const allDates=articles?.map(article=>article?.created_at)
+   const allYears=[]
+
+  allDates?.map(date=>{
+    const newDate=new Date(date) 
+    allYears.push(newDate.getFullYear()) 
+
+  })
+
+  let uniqueDates = [...new Set(allYears)];
  
- console.log("🚀 ~ index ~ allDates:", allDates)
 
+  const manageDateFormate = (date) => {
+    const newDate = new Date(date);
+    const dateString = newDate.toDateString();
+    const dateArray = dateString.split(" ");
+    const day = dateArray[0];
+    const currentDate = dateArray[2];
+    const month = dateArray[1];
+    const year = dateArray[3];
 
-//  const allYears=[]
-
-// allDates?.map(date=>{
-//   const newDate=new Date(deactivated() {
-    
-//   },
-  
-// })
-
-  const manageDateFormate=(date)=>{
-    const newDate=new Date(date)
-    const dateString=newDate.toDateString()
-    const dateArray=dateString.split(" ")
-    const day=dateArray[0]
-    const currentDate=dateArray[2]
-    const month=dateArray[1]
-    const year=dateArray[3]
-    
-
-    return `${day},${currentDate}th ${month},${year}`
-  }
-
+    return `${day},${currentDate}th ${month},${year}`;
+  };
 
   return (
     <DashboardLayout>
@@ -79,7 +84,6 @@ const AllBlogsPage = () => {
         </span>
       </div>
 
-
       {/* main body start */}
       <div className="flex flex-col mt-10 relative ">
         {/* search and filer option start*/}
@@ -88,7 +92,9 @@ const AllBlogsPage = () => {
             {/* bulk actions start */}
             <div className="flex items-center gap-3">
               <select className="px-10 py-2 rounded" name="" id="">
-                <option className="bg-white text-black" value="">Bulk Actions</option>
+                <option className="bg-white text-black" value="">
+                  Bulk Actions
+                </option>
               </select>
               {/* <span className="border border-orange-700 px-3 rounded text-orange-700 cursor-pointer">
                 Apply
@@ -97,32 +103,37 @@ const AllBlogsPage = () => {
             {/* bulk actions ends */}
             <div className="flex flex-row gap-2 items-center justify-between">
               <div>
-              <select className="px-10 py-2 rounded" name="" id="">
-                <option value="">All Dates</option>
-                {/* {
-                  allYear?.map((year,i)=>(
+                <select className="px-10 py-2 rounded" name="" id="">
+                  <option value="">All Dates</option>
+                  {
+                  uniqueDates?.map((year,i)=>(
                     <option key={i} value={year}>{year}</option>
                   ))
-                } */}
-              </select>
+                }
+                </select>
               </div>
               <div className="flex items-center gap-4">
-                <select onChange={(e)=>setSelectedCategory(e.target.value)} className="px-10 py-2 rounded" name="" id="">
+                <select
+                  onChange={(e) => setSelectedCategory(e.target.value)}
+                  className="px-10 py-2 rounded"
+                  name=""
+                  id=""
+                >
                   <option value="">All Category</option>
-                  {
-                    categories?.map(cat=>(
-                      <option key={cat?.id} value={cat?.id}>{cat?.title}</option>
-                    ))
-                  }
+                  {categories?.map((cat) => (
+                    <option key={cat?.id} value={cat?.id}>
+                      {cat?.title}
+                    </option>
+                  ))}
                 </select>
                 {/* <span className="border border-orange-700  px-3 rounded text-orange-700 cursor-pointer">
                   Apply
                 </span> */}
               </div>
             </div>
-              <div onClick={()=>{
-
-               setAllBlogDelete(true)
+            <div
+              onClick={() => {
+                setAllBlogDelete(true);
                 // fetch(`${baseUrl}/articles/destroy/all`,{
                 //   method:'POST',
                 //   headers:{
@@ -135,12 +146,14 @@ const AllBlogsPage = () => {
                 //     fetchArticles()
                 //   }
                 // })
-              }} className=" cursor-pointer">
-                <div className="bg-red-600 flex items-center gap-2 w-[50%] lg:w-full  px-3 py-1 rounded-md text-white">
-                <IoTrashBin/>
+              }}
+              className=" cursor-pointer"
+            >
+              <div className="bg-red-600 flex items-center gap-2 w-[50%] lg:w-full  px-3 py-1 rounded-md text-white">
+                <IoTrashBin />
                 <span>Clear All</span>
-                </div>
               </div>
+            </div>
           </div>
         </div>
         {/* search and filer option end*/}
@@ -174,57 +187,87 @@ const AllBlogsPage = () => {
               </tr>
             </thead>
             <tbody>
-              {
-                articles.map(article=>{
-                  
-                  return (
-                    (
-                      <tr key={article.id} className="border-b border-black text-[14px]">
-                        <td className="px-3 py-3 text-center">{`${article?.title.slice(0,10)}...`}</td>
-                        <td className="px-3 py-3 text-center">
-                          <span className="w-full flex justify-center"><img className="w-16 h-12 object-cover" src={article?.thumbnail} alt="" /></span>
-                        </td>
-                        <td className="px-3 py-3 text-center">
-                          {article?.category_id==1?"Movie":article?.category_id==2?"Tv Shows":article?.category_id==3?"Web Series":""}
-                        </td>
-                        <td className="px-3 py-3 text-center">
-                          {article?.genres.map((genre,i)=>(
-                            <div className="inline p-1" key={i}>{!genre?"N/A":genre?.title}</div>
-                          ))}
-                        </td>
-                        <td className="px-3 py-3 text-center">
-                          {`${article?.tags? `${article?.tags?.slice(0,10)}..`:"N/A"}`}
-                        </td>
-                        <td className="px-3 py-3 text-center">
-                          {manageDateFormate(article?.created_at)}
-                        </td>
-                        <td className="px-3 py-3 text-center flex gap-2 items-center justify-center h-full">
-                          <span onClick={()=>{
-                          
-                            setIsDeleteModalOpen({
-                              value:article?.id,status:true,from:"all"
-                            })
-                            setCurrentSelectedArticle(article)
-                          }}
-                          
-                          className="bg-red-500 px-3 py-1 text-white font-bold rounded cursor-pointer">Delete</span>
-                          <span onClick={()=>router(`/dashboard/edit-blog/${article?.id}`)} className="bg-red-500 px-3 py-1 text-white font-bold rounded cursor-pointer">Edit</span>
-                          
-                          </td>
-                      </tr>
-                    )
-                  )
-                })
-              }
+              {articles.map((article) => {
+                return (
+                  <tr
+                    key={article.id}
+                    className="border-b border-black text-[14px]"
+                  >
+                    <td className="px-3 py-3 text-center">{`${article?.title.slice(
+                      0,
+                      10
+                    )}...`}</td>
+                    <td className="px-3 py-3 text-center">
+                      <span className="w-full flex justify-center">
+                        <img
+                          className="w-16 h-12 object-cover"
+                          src={article?.thumbnail}
+                          alt=""
+                        />
+                      </span>
+                    </td>
+                    <td className="px-3 py-3 text-center">
+                      {article?.category_id == 1
+                        ? "Movie"
+                        : article?.category_id == 2
+                        ? "Tv Shows"
+                        : article?.category_id == 3
+                        ? "Web Series"
+                        : ""}
+                    </td>
+                    <td className="px-3 py-3 text-center">
+                      {article?.genres.map((genre, i) => (
+                        <div className="inline p-1" key={i}>
+                          {!genre ? "N/A" : genre?.title}
+                        </div>
+                      ))}
+                    </td>
+                    <td className="px-3 py-3 text-center">
+                      {`${
+                        article?.tags
+                          ? `${article?.tags?.slice(0, 10)}..`
+                          : "N/A"
+                      }`}
+                    </td>
+                    <td className="px-3 py-3 text-center">
+                      {manageDateFormate(article?.created_at)}
+                    </td>
+                    <td className="px-3 py-3 text-center flex gap-2 items-center justify-center h-full">
+                      <span
+                        onClick={() => {
+                          setIsDeleteModalOpen({
+                            value: article?.id,
+                            status: true,
+                            from: "all",
+                          });
+                          setCurrentSelectedArticle(article);
+                        }}
+                        className="bg-red-500 px-3 py-1 text-white font-bold rounded cursor-pointer"
+                      >
+                        Delete
+                      </span>
+                      <span
+                        onClick={() =>
+                          router(`/dashboard/edit-blog/${article?.id}`)
+                        }
+                        className="bg-red-500 px-3 py-1 text-white font-bold rounded cursor-pointer"
+                      >
+                        Edit
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
-
         </div>
-        
-          {/* deleteModal */}
-          {
-           ( isDeleteModalOpen.status || allBlogDelete) && <div className="absolute top-[25%] left-[15%] lg:left-[25%]"><BlogDeleteModal article={currentSelectedArticle}/></div>
-          }
+
+        {/* deleteModal */}
+        {(isDeleteModalOpen.status || allBlogDelete) && (
+          <div className="absolute top-[25%] left-[15%] lg:left-[25%]">
+            <BlogDeleteModal article={currentSelectedArticle} />
+          </div>
+        )}
 
         {/* show table with available blogs end */}
       </div>
